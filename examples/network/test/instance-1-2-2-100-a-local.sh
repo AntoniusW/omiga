d235f919e60d0537ae33d1b1d641c91c4ba5444b
Minh Dao-Tran <dao@kr.tuwien.ac.at>
2012-05-16 21:33:45 +0000

    Add a problematic test case.

---
+++ examples/network/test/instance-1-2-2-100-a-local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# start this file from WOC base directory
+export BASEDIR=.
+export CLASSPATH=$BASEDIR/build/classes/:$BASEDIR/lib/antlr-3.4-complete.jar:$BASEDIR/lib/jgrapht-0.8.3/jgrapht-jdk1.6.jar
+export INPUTFILE=$BASEDIR/examples/network/test/instance-1-2-2-100-a.lp
+
+time java -cp $CLASSPATH woc.classinstances.main_FINAL $INPUTFILE
