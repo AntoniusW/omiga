bfc73a2450658ce29bfa5dc5fde6f892e10d23b4
Minh Dao-Tran <dao@kr.tuwien.ac.at>
2012-08-27 17:27:37 +0000

    Update run script.

--- run_test_lsolve.sh
+++ run_test_lsolve.sh
@@ -7,25 +7,26 @@ export GNUTIME="/usr/bin/time --verbose -o" # time command
 export TIMELIMIT="timelimit -p -s 1 -t $TIMEOUT -T 20"
 export TIMEFORMAT=$'\nreal\t%3R\nuser\t%3U\nsys\t%3S' # time format
 
-export BASEDIR=.
+export BASEDIR=..
 export CLASSPATH=$BASEDIR/build/classes/:$BASEDIR/lib/antlr-3.4-complete.jar:$BASEDIR/lib/jgrapht-0.8.3/jgrapht-jdk1.6.jar
 
 
-export ASPERIXPATH='.'
+export ASPERIXPATH='../../ASPeRiX0.2.4'
 export SICSTUSPATH='/home/staff/aweinz/sicstus4.2.1/bin'
 export CLINGOPATH='/home/staff/aweinz/src/clingo/clingo-3.0.4-x86-linux'
-export WOCPATH='.'
+export OMIGAPATH='.'
+export INSTANCES='instances.txt'
 
 export EMAIL=dao@kr.tuwien.ac.at
 
-DOTIMELIMIT=yes
+DOTIMELIMIT=no
 ULIMIT=no
 
-export TESTCASES="reach 3col birds"
+export TESTCASES="reach birds 3col"
 #export TESTCASES="birds"
 
 #                      reach     reachplus birds                           3col                           stratProg
-#declare -a test_names=(1 2 3 4 5 1 2 3 4 5 1000 10000 100000 200000 500000 10-18 20-38 30-64 40-91 50-114 10 100 200 300 400 500)
+#declare -a test_names=(1 2 3 4 5 1 2 3 4 5 1000 10000 100000 200000 500000 10-18 20-38 30-64 50-114 10 100 200 300 400 500)
 #declare -a no_test_names=(5 5 5 5 6)
 #declare -a start_test_names=(0 5 5 15 21)
 
@@ -34,8 +35,8 @@ export TESTCASES="reach 3col birds"
 #declare -a no_n_answers=(1 1 1 2 2)
 #declare -a start_n_answers=(0 1 2 3 5)
 
-#                      reach     reachplus birds                3col                 stratProg
-declare -a test_names=(1 2 3     1 2 3     1000 10000 100000    10-18 20-38 30-64    10 100 200)
+#                      reach     birds                3col                 reachplus      stratProg
+declare -a test_names=(1 2 3     1000 10000 100000    10-18 20-38 30-64    1 2 3          10 100 200)
 declare -a no_test_names=(3 3 3 3 3)
 declare -a start_test_names=(0 3 6 9 12)
 
@@ -87,10 +88,10 @@ function runTestWithEngine()
 
     echo >> $LOGSTATS
 
-    if [ $RETVALUE = 0 ] ; then
+    if [ $RETVALUE = 0 -o $RETVALUE = 10 ] ; then
 	echo "PASS: $INPUT" >> $LOGSTATS
 	(echo "Test $LOG finished" ; echo ; cat $LOGTIME ; echo ; cat $LOGOUT) | mail -s "Test $LOG finisihed" $EMAIL
-    else
+    else 
 	echo "FAILED: $INPUT" >> $LOGSTATS
 	(echo "Test $LOG FAILED" ; echo ; cat $LOGTIME ; echo ; cat $LOGOUT) | mail -s "Test $LOG FAILED" $EMAIL
     fi
@@ -112,16 +113,16 @@ function runTestWithEngine()
 ##########################################################################
 function runTest()
 {
-    #RUNCOMMAND="$ASPERIX n $N -N 500000 $INPUT-hide.txt"
-    #runTestWithEngine $OUTPUT-asperix
+#    RUNCOMMAND="$ASPERIX n $N -N 500000 $INPUT-hide.txt"
+#    runTestWithEngine $OUTPUT-asperix
 
     #RUNCOMMAND="$DLV $INPUT.txt -filter=nothing -n=$N"
     #runTestWithEngine $OUTPUT-dlv
 
-    RUNCOMMAND="$CLINGO $INPUT.txt -n $N -q"
-    runTestWithEngine $OUTPUT-clingo
+#    RUNCOMMAND="$CLINGO $INPUT.txt -n $N -q"
+#    runTestWithEngine $OUTPUT-clingo
 
-    RUNCOMMAND="$OMIGA $INPUT.txt $N -nooutput"
+    RUNCOMMAND="$OMIGA $INPUT.txt $N"
     runTestWithEngine $OUTPUT-omiga
 
     #RUNCOMMAND="$SICSTUS -l gasp_test.pl -- $INPUT.txt"
@@ -131,7 +132,7 @@ function runTest()
 ########## check GNUTIME ################################################
 
 $GNUTIME /dev/null ls > /dev/null 2>&1
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
     echo I need GNU time, please setup GNUTIME properly. Bailing out.
     exit 1
 fi
@@ -142,6 +143,10 @@ declare -i i=0
 declare -i j=0
 declare -i k=0
 
+if [ x$1 != xappend ] ; then
+    > $INSTANCES
+fi
+
 for TEST in $TESTCASES ; do
     echo $TEST
     
@@ -150,8 +155,9 @@ for TEST in $TESTCASES ; do
     let "end_now += ${no_test_names[$i]}"
 
     for (( j=$start_now; j < $end_now; ++j )) ; do
-	INPUT=testLocalSolver/input/$TEST/$TEST-${test_names[$j]}
-	#echo $INPUT
+	INPUT=input/$TEST/$TEST-${test_names[$j]}
+	echo $INPUT
+	echo output/$TEST/$TEST-${test_names[$j]} >> $INSTANCES
 
 	# iterate over the number of answers to request
 	declare -i start_case=${start_n_answers[$i]}
@@ -160,7 +166,8 @@ for TEST in $TESTCASES ; do
 
 	for (( k=$start_case; k < $end_case; ++k )) ; do
 	    declare -i N=${n_answers[$k]}
-	    OUTPUT=testLocalSolver/output/$TEST/$TEST-${test_names[$j]}-$N
+	    OUTPUT=output/$TEST/$TEST-${test_names[$j]}-$N
+	    #echo $OUTPUT
 	    #echo $N
 
 	    runTest $INPUT $N $OUTPUT
